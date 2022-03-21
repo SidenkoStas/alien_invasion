7c5a02047600e22423ad4c4af4a2d0a7679d7452
SdienkoStas <vtv7htdv!>
2022-03-21 16:30:00 +0300

    Добавлены пули и их группировка. Реализовано движение:  - корабля;  - пулей.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,26 +1,31 @@
-import sys
 import pygame
+from pygame.sprite import Group
 from settings import Settings
+from ship import Ship
+from alien import Alien
+import game_functions as gf
 
 
-def run_game() -> object:
+def run_game():
     """Инициализирует pygame, settings и объект экрана."""
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode(
-        (ai_settings.screen_width,
-         ai_settings.screen_height)
-    )
+    screen = pygame.display.set_mode((ai_settings.screen_width,
+                                      ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    # Создание корабля.
+    ship = Ship(ai_settings, screen)
+    # Создание группы для хранения пуль.
+    bullets = Group()
+    # Создание кораблей врагов.
+    alien = Alien(ai_settings, screen)
 
     # Запуск основного цикла.
     while True:
-        # Отслеживание действий клавиатуры и мыши.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        # При каждом прохождении цикла перерисовывать фон.
-        screen.fill(ai_settings.bg_color)
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets, alien)
         # Отображение последнего присвоенного экрана.
         pygame.display.flip()
 
